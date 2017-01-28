3dc15311553073f280c8b7d48959d8c6d0fe428a
Hilton Rudham <Hilton Rudham>
2017-01-28 14:30:53 +0200

    Fixed hotcues

--- source/mappings/controls/performance-pads/hot-cue.js
+++ source/mappings/controls/performance-pads/hot-cue.js
@@ -6,8 +6,8 @@ for (var channelIndex = 0; channelIndex < 4; channelIndex++)
 	
 	for (var padIndex = 0; padIndex < 8; padIndex++)
 	{
-		var activateKey = 'hotcue_' + (channelIndex + 1) + '_activate';
-		var clearKey = 'hotcue_' + (channelIndex + 1) + '_clear';
+		var activateKey = 'hotcue_' + (padIndex + 1) + '_activate';
+		var clearKey = 'hotcue_' + (padIndex + 1) + '_clear';
 		manager.add(group, activateKey, 0x97 + channelIndex, 0x00 + padIndex);
 		manager.add(group, clearKey, 0x97 + channelIndex, 0x08 + padIndex);
 	}
